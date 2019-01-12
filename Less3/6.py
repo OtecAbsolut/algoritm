6ad3f68cb10d85af72904d265ecfcb4c407086f6
OtecAbsolut <s@kyky.cloud>
2019-01-12 09:15:54 +0300

    Д\з урок c исправлениями №3

--- Less3/6.py
+++ Less3/6.py
@@ -5,15 +5,15 @@
 import random
 
 MAX = 100
-MIN = 1
-COLUMNS = 5
-ROWS = 5
+MIN = -100
+COLUMNS = 10
+ROWS = 10
 
 test_matrix = [[random.randint(MIN, MAX) for _ in range(COLUMNS)] for _ in range(ROWS)]
 
 # генерим список будущих миниальных элементов
-min_num =[MAX for _ in range(COLUMNS)]
-max_min = 0
+min_num =[float('inf') for _ in range(len(test_matrix))]
+max_min = float('-inf')
 print('Исходная матрица:\n'
       '-------------------')
 for row in test_matrix:
@@ -27,5 +27,6 @@ print(min_num)
 for i in min_num:
     if i > max_min:
         max_min = i
-print(f'Максимальное среди минимальных: {max_min}')
+
+print(f'Максимальное среди минимальных: {max_min}\n')
 
